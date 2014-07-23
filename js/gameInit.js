c0476dfac555fb52770d4c9ccc5938f167df21fd
= <=>
2014-07-23 19:10:19 +0800

    添加效果

--- js/gameInit.js
+++ js/gameInit.js
@@ -4,10 +4,12 @@ var CJS = createjs,
 	bgBlur,				//遮罩层
 	bg,					//背景层
 	image,				
+
 	startShape, 		//开始按钮
 	radiusStep = 10,	//按钮圆角大小
-	speed = 1.5, 			//速度
-	shipNum = 6,		//默认战斗机数据
+	speed = 1.5, 		//速度
+	vy = 0.001, 		//加速度
+	shipNum = 8,		//默认战斗机数据
 	ships = [],			//战斗机数组
 	temp = [],
 	isOverrider = false,
@@ -20,6 +22,12 @@ var CJS = createjs,
 
 	ship, 				//战斗机
 	shipCon,			//找都记容器
+	bomb, 				//爆炸
+
+	bgVolum = 0.8, 		//背景音效大小
+	settingCon, 		//设置面板
+	setBgVolumBtn, 		//游戏音效音量
+	gameSettingBtn, 	//设置按钮
 
 	scoeTotal = 0,		//总分
 	scoeStep = 100,		//摧毁一个的的份
@@ -29,6 +37,10 @@ var CJS = createjs,
 	endTime,			//介绍记时
 	timeSprite,			//计时块
 	timeSpriteHeight = 5, //计时块高度
+	timeStep = 0.0003, 	//时间刻度
+	timeSheet, 			//效果
+
+	startTxt = '开始游戏吧，GO！',
 
 	musicPath = 'music/',
 	imagePath = 'images/',
@@ -63,12 +75,23 @@ window.onload = function(){
 
 	//--------------------------
 
+	//创建加载进度条
 	createLoading();
+	//加载资源
 	loadQueues();
 
 	CJS.Ticker.addEventListener('tick', loadTicker);
 	CJS.Ticker.setFPS(60);
 	CJS.Ticker.timingMode = CJS.Ticker.RAF;
+
+	//游戏设置
+	settingCon = document.getElementById('gameSettingCon');
+	setBgVolumBtn = document.getElementById('bgVolum');
+	gameSettingBtn = document.getElementById('gameSettingBtn');
+	var closeSettingBtn = document.getElementById('closeSettingBtn');
+	gameSettingBtn.addEventListener('touchend', gameSetting);
+	closeSettingBtn.addEventListener('touchend', closeSetting);
+	setBgVolumBtn.addEventListener('change', setBgVolum);
 
 };
 
@@ -86,6 +109,63 @@ function loadTicker(evt){
 }
 
 /**
+ * 游戏设置
+ * @param  {[type]} evt [description]
+ * @return {[type]}     [description]
+ */
+function gameSetting(evt){
+	showHideSettingCon(true);
+}
+function closeSetting(evt){
+	showHideSettingCon(false);
+}
+function showHideSettingCon(flag){
+	if(flag){
+		settingCon.style.display = 'block';
+		settingCon.style.opacity = '1';
+	}else{
+		settingCon.style.display = 'none';
+		settingCon.style.opacity = '0';
+	}
+}
+function setBgVolum(evt){
+	var value = evt.target.value;
+	document.getElementById('bgVolumValue').innerText = parseInt(value * 100) + '';
+	bgVolum = value;
+}
+
+/**
+ * 加载资源
+ * @return {[type]} [description]
+ */
+function loadQueues(){
+	//加载音乐
+	//var item = {src: musicPath + "18-machinae_supremacy-lord_krutors_dominion.ogg", id: "music"};
+	var manifest = [
+		{id: 'bgMusic', src: musicPath + "18-machinae_supremacy-lord_krutors_dominion.ogg"},
+		{id: 'breakMusic', src: musicPath + "Game-Break.ogg"},
+		{id: 'startMusic', src: musicPath + "Game-Start.ogg"},
+		{id: 'bgImage', src: imagePath + "bg.jpg"},
+		{id: 'shipImage', src: imagePath + "ship.png"},
+		{id: 'bombImage', src: imagePath + "bomb.png"},
+		{id: 'timeImage', src: imagePath + "timeSprite.png"}
+	];
+	// Instantiate a queue.
+    queue = new CJS.LoadQueue();
+	CJS.Sound.alternateExtensions = ["mp3"];
+	queue.installPlugin(CJS.Sound);
+    queue.addEventListener("complete", loadComplete);
+    queue.addEventListener("fileload", fileComplete);
+    queue.addEventListener("error",handleFileError);
+    queue.addEventListener("progress",handleProgress);
+    for (var i = manifest.length - 1; i >= 0; i--) {
+    	queue.loadFile(manifest[i], true);
+    };
+    
+    //queue.loadManifest(manifest);
+}
+
+/**
  * 资源加载完成
  * @param  {[type]} evt [description]
  * @return {[type]}     [description]
@@ -96,7 +176,8 @@ function loadComplete(evt) {
 	CJS.Ticker.removeEventListener('tick', loadTicker);
 
 	loadingBar.scaleX = 1;
-	loadingSquer.x = cw + 100;
+	loadingSquer.x = cw + 200;
+	loadingSquer.alpha = 0;
 	var loadMc = stage.getChildByName('_loadMc');
 	stage.removeChild(loadMc);
 	//设置背景
@@ -111,6 +192,10 @@ function fileComplete(evt){}
 function handleFileError(evt){}
 function handleProgress(evt){
 	loadingField.text = 'Loading: ' + (queue.progress * 100 | 0) + '%';
+	if(queue.progress == 1){
+		loadingBar.scaleX = 1;
+		loadingSquer.alpha = 0;
+	}
 	loadingBar.scaleX = queue.progress;
 	loadingSquer.x = queue.progress * cw;
 	stage.update();
@@ -131,17 +216,19 @@ function stopSound(name){
  */
 function setBackground(evt){
 	var bitmap = new CJS.Bitmap(bg);
-	bitmap.width = cw;
-	bitmap.height = ch;
+	bitmap.width = bitmap.image.width * (cw / bitmap.image.width );
+	bitmap.height = bitmap.image.height * (ch / bitmap.image.height );
 	bitmap.x = 0;
 	bitmap.y = 0;
+	bitmap.scaleX = (cw / bitmap.image.width); //bitmap.image.width * (bitmap.image.width / cw );
+	bitmap.scaleY = (ch / bitmap.image.height); //bitmap.image.height * (bitmap.image.height / cw );
 
 	//创建遮罩
 	bgBlur = createBlur();
 
 	//创建文本
 	startBtnCon = new CJS.Container();
-	startGameTXT = createTxt();
+	startGameTXT = createTxt(startTxt);
 	startGameTXT.filters = [new CJS.BlurFilter(20, 20, 10)];
 	startBtnCon.addChild(startGameTXT);
 	//创建按钮
@@ -155,43 +242,17 @@ function setBackground(evt){
 	stage.update();
 }
 
-function loadQueues(){
-	//加载音乐
-	//var item = {src: musicPath + "18-machinae_supremacy-lord_krutors_dominion.ogg", id: "music"};
-	var manifest = [
-		{id: 'bgMusic', src: musicPath + "18-machinae_supremacy-lord_krutors_dominion.ogg"},
-		{id: 'breakMusic', src: musicPath + "Game-Break.ogg"},
-		{id: 'startMusic', src: musicPath + "Game-Start.ogg"},
-		{id: 'bgImage', src: imagePath + "bg.jpg"},
-		{id: 'shipImage', src: imagePath + "ship.png"}
-	];
-	// Instantiate a queue.
-    queue = new CJS.LoadQueue();
-	CJS.Sound.alternateExtensions = ["mp3"];
-	queue.installPlugin(CJS.Sound);
-    queue.addEventListener("complete", loadComplete);
-    queue.addEventListener("fileload", fileComplete);
-    queue.addEventListener("error",handleFileError);
-    queue.addEventListener("progress",handleProgress);
-    //queue.loadFile(item, true);
-    queue.loadManifest(manifest);
-}
-
-
 //开始游戏
 function startGame(evt){
 	var target = evt.currentTarget;
 	var startShape = target.getChildByName('_startShape');
 	var startShapeTxt = target.getChildByName('_startShapeTxt');
-	// console.log(startShape);
 	
-	isOverrider = false;
-	scoeTotal = 0;
-	speed = 1;
-	ships = [];
-	temp = [];
-	scoeTxt = null;
+	//重置全局参数
+	resetGlobal();
 	
+	//清理舞台
+	gameSettingBtn.style.display = 'none';
 	stage.removeChild(startBtnCon);
 	stage.removeChild(bgBlur);
 	stage.update();
@@ -210,17 +271,18 @@ function startGame(evt){
 
 	//播放音效
 	var startMusic = playSound('startMusic');
-	var bgMusic = playSound('bgMusic');
-	bgMusic.setVolume(0.1);
+	if(bgVolum > 0){
+		var bgMusic = playSound('bgMusic');
+		bgMusic.setVolume(bgVolum);
+	}
 	//console.log(bgMusic.getVolume())
-	
+
+	//添加动画
 	CJS.Ticker.addEventListener("tick", tick);
 	CJS.Ticker.setFPS(60);
 	CJS.Ticker.timingMode = CJS.Ticker.RAF;
 
-	/*setTimeout(function(){
-		CJS.Ticker.removeEventListener('tick', tick);
-	}, 8000);*/
+	startShape.removeEventListener('click', startGame);
 
 	stage.update();
 }
@@ -235,10 +297,10 @@ function startGame(evt){
  */
 function tick(evt){
 	//加速
-	if(scoeTotal > 1000){
-		speed += 0.0005;
-	}
-	if(speed > 2.5) speed = 2.5;
+	//if(scoeTotal > 1000){
+		speed += vy;
+	//}
+	if(speed > 6.5) speed = 6.5;
 
 	//控制战斗机移动
 	var len = ships.length;
@@ -250,11 +312,11 @@ function tick(evt){
 				if(ships[i].y < -120){
 					ships[i].y = ch + Math.random() * 500;
 				}
-				ships[i].y -= Math.random() + speed;
+				ships[i].y -= Math.floor(Math.random() + speed);
 			}
 			stage.update();
 		}
-		//console.log(ships)
+		//console.log(speed)
 	}
 	//时间控制
 	var shapeTime = timeSprite.getChildByName('_shapeTime');
@@ -262,31 +324,30 @@ function tick(evt){
 	if(shapeTime.scaleX <= 0){
 		stop();
 	}else{
-		if(shapeTime.scaleX < 0.3)
-			shapeTime.scaleX -= 0.0002;
+		/*if(shapeTime.scaleX < 0.3)
+			shapeTime.scaleX -= 0.0003;
 		else
-			shapeTime.scaleX -= 0.0005;
+			shapeTime.scaleX -= 0.0005;*/
+		shapeTime.scaleX -= 0.0003;
+		timeSheet.x = shapeTime.scaleX * cw - 10;
 	}
-
+	//console.log('ships.length = ' + ships.length)
 }
 
 /**
  * 停止动画
- * @param  {[type]} evt [description]
  * @return {[type]}     [description]
  */
-function stop(evt){
+function stop(){
+	speed = 1.5;
+	ships = [];
+	temp = [];
 	//移出侦听事件
 	CJS.Ticker.removeEventListener('tick', tick);
-	startShape.removeEventListener('click', startGame);
-	stage.removeChild(shipCon);
-	stage.removeChild(scoeTxt);
-	stage.removeChild(timeSprite);
-	stage.update();
-
+	//清理舞台不需要的元素
+	clearStage();
 	//移出音效
 	stopSound('bgMusic');
-
 	//再来一次
 	gameAgain();
 	
@@ -327,7 +388,7 @@ function shipPong(evt){
 	//console.log(evt.currentTarget)
 	var target = evt.currentTarget;
 	var index = target.name.match(/\d/)[0];
-	shipCon.removeChild(evt.currentTarget);
+	shipCon.removeChild(target);
 	var ship = createShip(index);
 	resetBitShip(ship);
 	ships[index] = ship;
@@ -339,10 +400,47 @@ function shipPong(evt){
 	}else if(scoeTxt >= 5000){
 		scoeTxt.color = '#E81741';
 	}
-	//stage.update();
+	//加速------------
+	//speed += vy;
+	//播放爆炸声
 	var breakMusic = playSound('breakMusic');
+
+	//创建爆炸Sprite
+	stage.removeChild(bomb);
+	bomb = null;
+	delete bomb;
+	createBomb(target.x - target.image.width / 2, target.y - target.image.height / 2);
+	setTimeout(function(){
+		stage.removeChild(bomb);
+		stage.update();
+	}, 60);
 }
 
+/**
+ * 重置全局参数
+ * @return {[type]} [description]
+ */
+function resetGlobal(){
+	isOverrider = false;
+	scoeTotal = 0;
+	speed = 1.5;
+	ships = [];
+	temp = [];
+	scoeTxt = null;
+}
+
+function clearStage(){
+	stage.removeChild(shipCon);
+	stage.removeChild(scoeTxt);
+	stage.removeChild(timeSprite);
+	stage.removeChild(bomb);
+	timeSprite.removeChild(timeSheet);
+	shipCon = null;
+	scoeTxt = null;
+	timeSprite = null;
+	bomb = null;
+	stage.update();
+}
 
 /**
  * 创建遮罩
@@ -373,8 +471,8 @@ function createShip(index){
 	bitship.regY = ship.height * .5;*/
 	//bitship.x = ship.width * .5 + Math.random() * (cw - ship.width * .5);
 	resetBitShip(bitship);
-	shipCon.addChild(bitship);
-	ships.push(bitship);
+	//shipCon.addChild(bitship);
+	//ships.push(bitship);
 	bitship.addEventListener('click', shipPong);
 	return bitship;
 }
@@ -391,7 +489,9 @@ function createShips(){
 	var len = shipNum;
 
 	for(; i < len; ++i){
-		createShip(i);
+		var ship = createShip(i);
+		shipCon.addChild(ship);
+		ships.push(ship);
 		//bitship.addEventListener('tick', shipMove);
 	}
 	stage.update();
@@ -482,7 +582,7 @@ function createStartBtn(){
 	startShape.mouseEnabled = true;
 	startShape.name = '_startShape';
 	//按钮文字
-	var txt = isOverrider ? 'Go Again' : 'Go';
+	var txt = isOverrider ? 'Go Again' : 'Start';
 	var tw = isOverrider
 	var startShapeTxt = new CJS.Text(txt, 'bold 28px Arial', '#333');
 	startShapeTxt.lineWidth = sw;
@@ -505,9 +605,46 @@ function createTimeSprite(){
 	var mc = new CJS.Container();
 	mc.x = 0;
 	mc.y = 0;
-	var shapeBG = new CJS.Shape(new CJS.Graphics().beginFill('rgba(255, 255, 255, .6)').drawRect(0, 0, cw, timeSpriteHeight));
+	var shapeBG = new CJS.Shape(new CJS.Graphics().beginFill('rgba(255, 255, 255, .5').drawRect(0, 0, cw, timeSpriteHeight));
 	var shapeTime = new CJS.Shape(new CJS.Graphics().beginFill('rgba(255, 4, 84, .8)').drawRect(0, 0, cw, timeSpriteHeight));
 	shapeTime.name = '_shapeTime';
-	mc.addChild(shapeBG, shapeTime);
+	createTimeSheet(cw, -18);
+	mc.addChild(shapeBG, shapeTime, timeSheet);
 	return mc;
+}
+
+/**
+ * 创建爆炸效果
+ * @param  {[type]} x [description]
+ * @param  {[type]} y [description]
+ * @return {[type]}   [description]
+ */
+function createBomb(x, y){
+	var data = new CJS.SpriteSheet({
+			"images": [queue.getResult("bombImage")],
+			"frames": {"regX": 30, "height": 120, "count": 19, "regY": 30, "width": 120},
+			// define two animations, run (loops, 1.5x speed) and jump (returns to run):
+			"animations": {"run": [0, 19, "run", .5]}
+		});
+	bomb = new CJS.Sprite(data, "run");
+	bomb.setTransform(x || 0, y || 0, .8, .8);
+	bomb.framerate = 19;
+	//bomb.regX = (cw - bomb.width) * .5 - bomb.width;
+	//bomb.regY = (ch - bomb.height) * .5 - bomb.height;
+	stage.addChild(bomb);
+	stage.update();
+}
+
+function createTimeSheet(x, y){
+	var data = new CJS.SpriteSheet({
+			"images": [queue.getResult("timeImage")],
+			"frames": {"regX": 0, "height": 50, "count": 20, "regY": 0, "width": 100},
+			"animations": {"run": [0, 20, "run", .5]}
+		});
+	timeSheet = new CJS.Sprite(data, "run");
+	timeSheet.setTransform(x || cw + 10, y || 0, .8, .8);
+	timeSheet.framerate = 20;
+	timeSheet.name = '_timeSheet';
+	stage.update();
+	//return timeSheet;
 }
